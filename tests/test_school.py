28e5aa17067d9a0e11578239a590c4fba2c532bf
Nicolas Bock <nicolasbock@gmail.com>
2022-04-15 07:39:45 -0600

    Add more unit tests for School
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- tests/test_school.py
+++ tests/test_school.py
@@ -23,6 +23,18 @@ class TestSchool(unittest.TestCase):
         nearest_neighbors = self.school.get_nearest_neighbors(self.fishies[1])
         self.assertEqual(nearest_neighbors[0], self.fishies[2])
 
+    def test_add_fish(self):
+        """Test adding a fish to the school."""
+        new_fish = Fish(20, 20)
+        self.school.add_fish(new_fish)
+        self.assertEqual(self.school.get_fish()[-1], new_fish)
+
+    def test_get_fish(self):
+        """Test getting the fishies."""
+        fishies = self.school.get_fish()
+        for i, fish in enumerate(fishies):
+            self.assertEqual(fish, self.fishies[i])
+
 
 if __name__ == '__main__':
     unittest.main()
